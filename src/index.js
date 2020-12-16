a05844a93345db9bd917bb67b73c7f8d928eabe6
Nika <yavtushenkonika@gmail.com>
2020-12-16 19:00:45 +0200

    change coments

--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
 import './styles.css';
 import refs from './js/refs';
-// import { setInterval } from 'core-js';
-
 // // console.log(refs);
-
-// let christmasDay = `Dec 25 2020`
-
-
-// function mathTime(time) {
-//     const days = Math.floor(time / (1000 * 60 * 60 * 24));
-//     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-//     const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-//     return { days, hours, mins, secs };
-// };
-
-// function myTimer(date) {
-//     let timeBetween = new Date(date) - Date.now();
-//     // console.log(timeBetween);
-//     const x = mathTime(timeBetween);
-    
-
-//     refs.days.textContent = x.days;
-//     refs.hours.textContent = x.hours;
-//     refs.mins.textContent = x.mins;
-//     refs.secs.textContent = x.secs;
-// }
-
-
-//  setInterval(() => {
-//     myTimer(christmasDay);
-// }, 1000)
-
 
 class CountdownTimer {
     constructor({selector, targetDate}) {
@@ -91,3 +59,32 @@ const christmassDay = new CountdownTimer({
 console.log(christmassDay.targetDate)
 
 christmassDay.start();
+
+// let christmasDay = `Dec 25 2020`
+
+
+// function mathTime(time) {
+//     const days = Math.floor(time / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+//     const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+//     return { days, hours, mins, secs };
+// };
+
+// function myTimer(date) {
+//     let timeBetween = new Date(date) - Date.now();
+//     // console.log(timeBetween);
+//     const x = mathTime(timeBetween);
+    
+
+//     refs.days.textContent = x.days;
+//     refs.hours.textContent = x.hours;
+//     refs.mins.textContent = x.mins;
+//     refs.secs.textContent = x.secs;
+// }
+
+
+//  setInterval(() => {
+//     myTimer(christmasDay);
+// }, 1000)
