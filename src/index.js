7cd3734604bd0762ac1ae08682e02a789b90621f
Nika <yavtushenkonika@gmail.com>
2020-12-19 14:08:47 +0200

    adds prettier

--- src/index.js
+++ src/index.js
@@ -3,65 +3,63 @@ import refs from './js/refs';
 // // console.log(refs);
 
 class CountdownTimer {
-    constructor({selector, targetDate}) {
-        this.targetDate = targetDate;
-        this.timer = document.getElementById(selector);
-        this.days = document.querySelector('span[data-value="days"]');
-        this.hours = document.querySelector('span[data-value="hours"]');
-        this.mins = document.querySelector('span[data-value="mins"]');
-        this.secs = document.querySelector('span[data-value="secs"]');
+	constructor({ selector, targetDate }) {
+		this.targetDate = targetDate;
+		this.timer = document.getElementById(selector);
+		this.days = document.querySelector('span[data-value="days"]');
+		this.hours = document.querySelector('span[data-value="hours"]');
+		this.mins = document.querySelector('span[data-value="mins"]');
+		this.secs = document.querySelector('span[data-value="secs"]');
+	}
 
-    }
+	start() {
+		this.myTimer();
 
-    start() {
-        this.myTimer();
+		setInterval(() => {
+			this.myTimer();
+		}, 1000);
+	}
 
-        setInterval(() => {
-        this.myTimer();
-        }, 1000)
-    }
-    
-    myTimer() {
-        let timeBetween = this.targetDate - Date.now();
-        const x = this.mathTime(timeBetween);
+	myTimer() {
+		let timeBetween = this.targetDate - Date.now();
+		const x = this.mathTime(timeBetween);
 
-        // console.log(this.targetDate)
-        // console.log(Date.now())
-        // console.log(timeBetween)
-        // console.log(x)
+		// console.log(this.targetDate)
+		// console.log(Date.now())
+		// console.log(timeBetween)
+		// console.log(x)
 
-        this.days.textContent = x.days;
-        this.hours.textContent = x.hours;
-        this.mins.textContent = x.mins;
-        this.secs.textContent = x.secs;
+		this.days.textContent = x.days;
+		this.hours.textContent = x.hours;
+		this.mins.textContent = x.mins;
+		this.secs.textContent = x.secs;
+	}
+	mathTime(time) {
+		const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+		const hours = this.pad(
+			Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+		);
+		const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+		const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-    }
-    mathTime(time) {
-        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+		return { days, hours, mins, secs };
+	}
 
-    return { days, hours, mins, secs };
-    }
-
-    pad(value) {
-        return String(value).padStart(2, '0')
-    }
+	pad(value) {
+		return String(value).padStart(2, '0');
+	}
 }
 
-
 const christmassDay = new CountdownTimer({
-  selector: '#timer-1',
-    targetDate: new Date('Dec 25, 2020'),
+	selector: '#timer-1',
+	targetDate: new Date('Dec 25, 2020'),
 });
 
-console.log(christmassDay.targetDate)
+console.log(christmassDay.targetDate);
 
 christmassDay.start();
 
 // let christmasDay = `Dec 25 2020`
-
 
 // function mathTime(time) {
 //     const days = Math.floor(time / (1000 * 60 * 60 * 24));
@@ -76,14 +74,12 @@ christmassDay.start();
 //     let timeBetween = new Date(date) - Date.now();
 //     // console.log(timeBetween);
 //     const x = mathTime(timeBetween);
-    
 
 //     refs.days.textContent = x.days;
 //     refs.hours.textContent = x.hours;
 //     refs.mins.textContent = x.mins;
 //     refs.secs.textContent = x.secs;
 // }
-
 
 //  setInterval(() => {
 //     myTimer(christmasDay);
